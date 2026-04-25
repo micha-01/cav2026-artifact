b8e02a0be31f6a6f018d600652cf3235a75f2c67
Micha <>
2026-04-25 14:36:43 +0200

    added setup script for VM

---
+++ setup_vm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# install dependencies and helpful tools
+echo "deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+
+sudo apt --yes update
+sudo apt --yes install code
+sudo apt --yes install git
+sudo apt --yes install openjdk-11-jdk
+sudo apt --yes install python3.12
+sudo apt --yes install python3-pip
+sudo apt --yes install python3.12-venv
+
+code --install-extension viper-admin.viper
+
+# install nagini
+git clone git@github.com:micha-01/Nagini.git
+cd Nagini
+git checkout object_equality
+pip3 install .
+cd ..
+
+# download other files in the artifact (logs, tutorial)
+git init
+git remote add origin https://github.com/micha-01/cav2026-artifact.git
+git fetch origin
+git checkout -b main --track origin/main
