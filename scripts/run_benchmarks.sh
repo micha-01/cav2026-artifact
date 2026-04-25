f2c4bf2990d128380136cb27dbb205f3e1525379
Micha <>
2026-04-25 16:22:44 +0200

    added env vars

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -3,6 +3,9 @@
 BENCHMARKS=$HOME/Nagini/benchmarks
 LOGS=$HOME/logs
 
+export BOOGIE_EXE=$HOME/.vscode/extensions/viper-admin.viper-5.3.2-linux-x64/dependencies/ViperTools/boogie/Binaries/Boogie
+export MYPYPATH=$HOME/Nagini/src
+
 mkdir -p $BENCHMARKS
 mkdir -p $LOGS
 
