40b40cf8cd7420a2184a3c7f589f34b08ffb2d16
Micha <>
2026-04-27 14:13:18 +0200

    updated nagini benchmark script

--- scripts/run_nagini_benchmarks.sh
+++ scripts/run_nagini_benchmarks.sh
@@ -2,6 +2,8 @@
 
 BENCHMARKS=$HOME/Nagini/benchmarks
 LOGS=$HOME/logs
+TIMESTAMP=$(date "+%Y%m%d_%H:%M:%S")
+OUTPUT_FILE="$LOGS/run_Nagini_$TIMESTAMP.txt"
 
 export BOOGIE_EXE=$HOME/.vscode/extensions/viper-admin.viper-5.3.2-linux-x64/dependencies/ViperTools/boogie/Binaries/Boogie
 export MYPYPATH=$HOME/Nagini/src
@@ -9,11 +11,11 @@ export MYPYPATH=$HOME/Nagini/src
 mkdir -p $BENCHMARKS
 mkdir -p $LOGS
 
-echo "Starting Benchmarks: $BENCHMARKS."
+echo "Starting Nagini Benchmarks: $BENCHMARKS." | tee "$OUTPUT_FILE"
+echo "--------------------------------------"
 
 i=0
-N=6
-TIMESTAMP=$(date "+%Y%m%d_%H:%M:%S")
+N=8
 FILES="$BENCHMARKS"/*.py
 NUM_FILES=$(ls $FILES | wc -l)
 for FILE in $FILES; do
@@ -24,6 +26,6 @@ for FILE in $FILES; do
         echo "# Benchmark $i: $FILENAME."
         nagini --benchmark $N --verifier=carbon --boogie $BOOGIE_EXE "$FILE"
         echo ""
-    } >> "$LOGS/run_$TIMESTAMP.txt"
+    } >> "$OUTPUT_FILE"
     echo "Done with Benchmark $i."
 done
